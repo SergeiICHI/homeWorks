23001e638e62d07275de141b9e6143ca7be19b53
sa <as>
2023-01-08 18:45:52 +0300

    done

---
+++ Task66 рекурсия/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+
+int summ(int n, int m)
+{
+    if(n == m) return n;
+    return n + summ(n+1,m);
+}
+
+
+Console.WriteLine("Введите 2 числа, чтобы узнать сумму чисел между ними");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел от {n} до {m} = {summ(n,m)}");
